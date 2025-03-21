43642a334e5606f33d64524d4fe16048f8640a4c
YashBaviskar1 <yashbav24@gmail.com>
2025-03-22 02:25:44 +0530

    schedule

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -24,11 +24,18 @@ import andhadhun from "../assets/andhadhun.jpg";
 import scatterball from "../assets/scatterball.jpg";
 import waitingroom from "../assets/waitingroom.jpg";
 import valo from "../assets/valo.jpg";
+import bgmi from "../assets/bgmi.jpg";
+import couch from "../assets/couch.jpg";
+import Ludo from "../assets/Ludo.jpg";
+import apsit from "../assets/apsit.jpg";
+import prize from "../assets/prize.jpg";
+import fifa from "../assets/fifa.jpg";
+
 import debate from "../assets/debate.jpg";
 import "./Schedule.css"
 
 const Schedule = () => {
-  const [selectedDay, setSelectedDay] = useState(0); 
+  const [selectedDay, setSelectedDay] = useState(0); // 0 for Day 1, 1 for Day 2
   const [activeTab, setActiveTab] = useState(null);
   const [isFading, setIsFading] = useState(false);
 
@@ -82,21 +89,40 @@ const Schedule = () => {
     // Day 2
     [
       [
-        { name: 'Rocket League', time: "11:00-03:00", image: rocket },
-        { name: 'Funfair', time: "11:00-03:00", image: funfair },
-        { name: 'Andhadhun', time: "11:00-05:00", image: andhadhun },
-        { name: 'Scatter Ball', time: "Full Day", image: scatterball },
+        { name: 'Mr & Ms APSIT ROUND1', time: "10:00-12:30", image: apsit },
+        { name: 'SPORTS SOLO Prize Distribution', time: "01:00-03:00", image:prize },
+        { name: 'Mr & Ms APSIT ROUND2', time: "03:00-05:00", image: apsit },
+        { name: 'fine arts', time: "12:00-3:00", image: arts },
+        { name: 'Karaoke(day 2)', time: "10:00-5:00", image: karaoke },
+        { name: 'Paint Ball(day2)', time: "10:00-05:00", image: paintball },
+        
       ],
       [
-        { name: 'Waiting Rooms', time: "Full Day", image: waitingroom },
-        { name: 'IPL Auction (Final)', time: "12:00-03:00", image: auction },
+        { name: 'Human Ludo', time: "Full Day", image: Ludo },
+        { name: 'IPL Auction round 2', time: "11:00-02:00", image: auction },
+        { name: 'Theme based-Doors of fate', time: "11:00-03:00", image: doors },
+        { name: 'Snake and ladder-twister', time: "11:00-05:00", image: snake },
+        { name: 'Horror Room', time: "12:00-03:00", image: horror },
+        {name:"Escape room",time:"11:00-04:00",image:escaperoom},
       ],
       [
-        { name: 'Treasure Hunt (Finals)', time: "11:00-03:00", image: hunt },
-        { name: 'Horror House', time: "12:00-05:00", image: horror },
+        { name: 'Treasure Hunt (Round 2)', time: "10:00-12:00", image: hunt },
+        { name: 'BGMI', time: "11:00-02:00", image: bgmi },
+        { name: 'Scatterball', time: "10:00-03:00", image: scatterball },
+        {name:"neon cricket",time:"10:00-03:00",image:neoncricket},
+        {name:"challenge carnival",time:"10:00-03:00",
+        image:carnival},
+        { name: 'Just Dance', time: "11:00-03:00", image: justdance },
+        {name:'Theme based-Hell portal v/s Heaven portal',time:"11:00-03:00",image:hellheaven},
+        {name:"Couch Games",time:"11:00-03:00",image:couch},
+        {name:"Squid Game",time:"11:00-03:00",image:squid},
+        { name: 'Waiting Room', time: "11:00-03:00", image: waitingroom },
+        { name: 'Rocket League', time: "12:00-03:00", image: rocket },
+        
       ],
       [
-        { name: 'Debate Finals', time: "2:00-4:00", image: debate },
+        { name: 'Valo', time: "11:00-5:00", image: valo },
+        {name:"Fifa",time:"11:00-5:00",image:fifa},
       ],
     ],
   ];
